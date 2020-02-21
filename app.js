3775c4a075cdf80007e821a944dd88e1e46dd049
Pei <stevenkaoo@gmail.com>
2020-02-21 16:39:24 +1100

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const Koa = require('koa');  
+const Logger = require('koa-logger')
+const Router = require('koa-router')
+const app = new Koa(); 
+const router = Router();
+
+router.get('/', async(ctx) => {     
+  ctx.body = 'Hello Koa body'; 
+});  
+
+router.get('/hello', async(ctx) => {     
+  ctx.body = 'world'; 
+});  
+
+app.use(Logger())
+app.use((ctx, next) => {   
+  const start_time = Date.now();   
+  return next().then(() => {     
+      const ms = Date.now() - start_time; 
+      console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);  
+  }); 
+});
+
+app.use(router.routes());  
+app.listen(3000);
